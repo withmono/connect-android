eebedae4d812243c69936e8ff2f74e4499bb2c8f
Tristan Tsvetanov <tristan@mono.co>
2021-06-10 05:12:35 -0400

    Revert "docs: update readme, add contributor guidlines, issue template, and CoC"
    This reverts commit 217c017ab371fb470320f64d896b9a96080b7074.

--- widget/src/main/java/mono/connect/kit/ConnectKit.java
+++ widget/src/main/java/mono/connect/kit/ConnectKit.java
@@ -31,10 +31,6 @@ public class ConnectKit {
       this.params.put(Constants.KEY_REFERENCE, config.reference);
     }
 
-    if(config.reauthCode != null){
-      this.params.put(Constants.KEY_REAUTH_TOKEN, config.reauthCode);
-    }
-
     if(config.onSuccess != null){
       MonoWebInterface.getInstance().setOnSuccess(config.onSuccess);
     }
@@ -59,6 +55,11 @@ public class ConnectKit {
   }
 
   public void show() {
+    this.startWidgetActivity();
+  }
+
+  public void reauthorise(String token) {
+    this.params.put(Constants.KEY_REAUTH_TOKEN, token);
     this.startWidgetActivity();
   }
 
